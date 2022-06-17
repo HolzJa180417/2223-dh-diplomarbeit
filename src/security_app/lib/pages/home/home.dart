c2651230bb4dc06959302da62f15450051a18d30
HolzJa180417 <40146720180417@litec.ac.at>
2022-06-17 10:58:39 +0200

    working protocols added

--- src/security_app/lib/pages/home/home.dart
+++ src/security_app/lib/pages/home/home.dart
@@ -1,1 +1,15 @@
 //export class for the whole home page, will import widgets and view export file
+import 'package:flutter/material.dart';
+
+class MyHomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Security App"),
+        centerTitle: true,
+      ),
+      body: Container(child: Text("Security")),
+    );
+  }
+}
--- src/security_app/lib/providers/navigation_provider.dart
+++ src/security_app/lib/providers/navigation_provider.dart
@@ -1,1 +1,6 @@
 //provides navigation states
+import 'package:flutter/material.dart';
+
+class Navigator with ChangeNotifier {
+  //functions for navigation....
+}
