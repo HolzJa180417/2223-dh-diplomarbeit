fbffea076102b4238fc9e871f29f843c12880ee5
HolzJa180417 <40146720180417@litec.ac.at>
2022-06-22 11:20:24 +0200

    php login script fehler behoben

--- src/php/login.php
+++ src/php/login.php
@@ -16,31 +16,44 @@
 
     $_email = mysqli_real_escape_string($con, $email);
 
-    //get password hash for given mail
-    $query = "SELECT password_hash FROM Registrierungsdaten WHERE email='{$email}'";
+    //check if mail even exists
+    $query = "SELECT * FROM Registrierungsdaten WHERE email='{$_email}'";
     $result = mysqli_query($con, $query);
-    $hashed_password = mysqli_fetch_array($result);
-    echo $hashed_password;
+
+    //if the email does not exist
+    if(mysqli_num_rows($result) == 0){
+      $msg = "email-not-exists-error" ;
+	    echo $msg;
+	    exit();
+    }
+
+    //get password hash for given mail
+    $query = "SELECT * FROM Registrierungsdaten WHERE email='{$email}'";
+    $result = mysqli_query($con, $query);
+    
+    while($row = mysqli_fetch_array($result)) {
+      $id = $row['id'];
+      $firstname = $row['firstName'];
+      $lastname = $row['lastName'];
+      $email = $row['email'];
+      $mobilenumber = $row['phoneNumber'];
+      $db_password = $row['password_hash'];
+      $gender = $row['gender'];
+      $hasPhoto = $row['hasPhoto'];
+      $age = $row['yearOfBirth'];
+      $code = $row['code'];
+    }
+
+    $_password = password_verify($password, $db_password);
 
     //if the email and password is correct
-    if(password_verify($password, strval($hashed_password))) {
+    if($_password == $password && $_email == $email) {
       $msg = "success" ;
 	    echo $msg;
 	    exit();
     }
     else{
       $msg = "password-not-correct-error" ;
-	    echo $msg;
-	    exit();
-    }
-
-    //check if mail even exists
-    $query = "SELECT * FROM Registrierungsdaten WHERE email='{$email}'";
-    $result = mysqli_query($con, $query);
-
-    //if the email does not exist
-    if(mysqli_num_rows($result) == 0){
-      $msg = "email-not-exists-error" ;
 	    echo $msg;
 	    exit();
     }
